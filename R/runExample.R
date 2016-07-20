a696c0727fc1eb870a0b27652cd2adeb53e284ff
Dean Attali <daattali@gmail.com>
2016-07-19 23:47:26 -0700

    start work on example shiny app

---
+++ R/runExample.R
@@ -0,0 +1,19 @@
+#' Run examples of using timelinevis in a Shiny app
+
+#' This example is also
+#' \href{http://daattali.com/shiny/timelinevis-demo/}{available online}.
+#'
+#' @examples
+#' if (interactive()) {
+#'   runExample()
+#' }
+#' @export
+runExample <- function() {
+  appDir <- system.file("example", package = "timelinevis")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `timelinevis`.",
+         call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
