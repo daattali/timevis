6508e7f8d38904e1b669de9d55c53733c86f7e66
Dean Attali <daattali@gmail.com>
2016-07-24 22:50:39 -0700

    update UI of shiny app

--- inst/example/ui.R
+++ inst/example/ui.R
@@ -39,46 +39,68 @@ fluidPage(
       div(icon("sliders"), "Fully interactive"),
       br(),
       fluidRow(
-        column(8,
-               timevisOutput("timelineInteractive")
+        column(
+          8,
+          fluidRow(column(12,
+            timevisOutput("timelineInteractive")
+          )),
+          fluidRow(
+            column(
+              12,
+              div(id = "interactiveActions",
+                  class = "optionsSection",
+                  tags$h4("Actions:"),
+                  actionButton("fit", "Fit all items"),
+                  actionButton("setWindowAnim", "Set window 2016-01-07 to 2016-01-25"),
+                  actionButton("setWindowNoAnim", "Set window without animation"),
+                  actionButton("center", "Center around 2016-01-23"),
+                  actionButton("focus2", "Focus item 4"),
+                  actionButton("focusSelection", "Focus current selection"),
+                  actionButton("addTime", "Add a draggable vertical bar 2016-01-17")
+              ),
+              br()
+            )
+          ),
+          fluidRow(
+            column(
+              4,
+              div(class = "optionsSection",
+                  uiOutput("selectIdsOutput", inline = TRUE),
+                  actionButton("selectItems", "Select"),
+                  checkboxInput("selectFocus", "Focus on selection", FALSE)
+              )
+            ),
+            column(
+              4,
+              div(class = "optionsSection",
+                  textInput("addText", tags$h4("Add item:"), "New item"),
+                  dateInput("addDate", NULL, "2016-01-15"),
+                  actionButton("addBtn", "Add")
+              )
+            ),
+            column(
+              4,
+              div(class = "optionsSection",
+                  uiOutput("removeIdsOutput", inline = TRUE),
+                  actionButton("removeItem", "Remove")
+              )
+            )
+          )
         ),
         column(4,
            div(
              id = "timelinedata",
              class = "optionsSection",
+             tags$h4("Data:"),
              tableOutput("table"),
              hr(),
-             div("Visible window:", textOutput("window", inline = TRUE)),
-             div("Selected items:", textOutput("selected", inline = TRUE))
+             div(tags$strong("Visible window:"),
+                 textOutput("window", inline = TRUE)),
+             div(tags$strong("Selected items:"),
+                 textOutput("selected", inline = TRUE))
            )
         )
-      ),
-      fluidRow(column(3,
-        div(id = "interactiveActions",
-          class = "optionsSection",
-          tags$h4("Actions:"),
-          actionButton("fit", "Fit all items"),
-          actionButton("setWindowAnim", "Set window 2016-01-07 to 2016-01-25"),
-          actionButton("setWindowNoAnim", "Set window without animation"),
-          actionButton("center", "Center around 2016-01-23"),
-          actionButton("focus2", "Focus item 4"),
-          actionButton("focusSelection", "Focus current selection"),
-          actionButton("addTime", "Add a draggable vertical bar 2016-01-17")
-      )),column(3,
-        div(class = "optionsSection",
-          uiOutput("selectIdsOutput", inline = TRUE),
-          actionButton("selectItems", "Select"),
-          checkboxInput("selectFocus", "Focus on selection", FALSE)
-      )),column(3,
-        div(class = "optionsSection",
-          textInput("addText", tags$h4("Add item:"), "New item"),
-          dateInput("addDate", NULL, "2016-01-15"),
-          actionButton("addBtn", "Add")
-      )),column(3,
-        div(class = "optionsSection",
-          uiOutput("removeIdsOutput", inline = TRUE),
-          actionButton("removeItem", "Remove")
-      )))
+      )
     )
   )
 )
