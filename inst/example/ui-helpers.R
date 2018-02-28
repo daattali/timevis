9a2ba407beed2b6b1519a3e4501d6c6e80f067be
Greg Minshall <minshall@acm.org>
2018-02-28 15:41:23 +0300

    keep example code consistent (?) with underlying code

--- inst/example/ui-helpers.R
+++ inst/example/ui-helpers.R
@@ -14,7 +14,7 @@ data <- data.frame(
   content = c("Item one", "Item two",
               "Ranged item", "Item four"),
   start   = c("2016-01-10", "2016-01-11",
-              "2016-01-20", "2016-02-14 15:00:00"),
+              "2016-01-20", "2016-02-14"),
   end     = c(NA, NA, "2016-02-04", NA)
 )
 
@@ -29,7 +29,7 @@ data <- data.frame(
   content = c("Item one", "Item two",
               "Ranged item", "Item four"),
   start   = c("2016-01-10", "2016-01-11",
-              "2016-01-20", "2016-02-14 15:00:00"),
+              "2016-01-20", "2016-02-14"),
   end     = c(NA, NA, "2016-02-04", NA)
 )
 
