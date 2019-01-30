af3ab5420a38b5e1b34c5ddad92be9227eaa1b27
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2019-01-30 13:57:00 +1100

    Added calls to info output helper

--- tests/testthat/test-dataframeToD3.R
+++ tests/testthat/test-dataframeToD3.R
@@ -8,21 +8,24 @@ test_that("dataframeToD3 works with no input", {
 test_that("dataframeToD3 works with a single row", {
   df <- data.frame(name = "Dean", age = 27, stringsAsFactors = FALSE)
   list <- list(list(name = "Dean", age = "27"))
-  expect_identical(dataframeToD3(df), list)
+  expect_identical(dataframeToD3(df), list,
+                   info = info_comp(dataframeToD3(df), list))
 })
 
 test_that("dataframeToD3 works with multiple rows", {
   df <- data.frame(name = c("Dean", "Ben"), age = c(27, 24))
   list <- list(list(name = "Dean", age = "27"),
                list(name = "Ben", age = "24"))
-  expect_identical(dataframeToD3(df), list)
+  expect_identical(dataframeToD3(df), list,
+                   info = info_comp(dataframeToD3(df), list))
 })
 
 test_that("dataframeToD3 works with NA values", {
   df <- data.frame(name = c("Dean", "Ben"), age = c(27, 24), degree = c("MSc", NA))
   list <- list(list(name = "Dean", age = "27", degree = "MSc"),
                list(name = "Ben", age = "24"))
-  expect_identical(dataframeToD3(df), list)
+  expect_identical(dataframeToD3(df), list,
+                   info = info_comp(dataframeToD3(df), list))
 })
 
 test_that("dataframeToD3 errors when given a non-dataframe", {
@@ -33,5 +36,6 @@ test_that("dataframeToD3 errors when given a non-dataframe", {
 test_that("dataframeToD3 returns the same whether the dataframe is pure or merged", {
   df <- data.frame(name = c("Dean", "Ben"), age = c(27, 24))
   df_rbind <- rbind(df[1, ], df[2, ])
-  expect_identical(dataframeToD3(df), dataframeToD3(df_rbind))
+  expect_identical(dataframeToD3(df), dataframeToD3(df_rbind),
+                   info = info_comp(dataframeToD3(df), dataframeToD3(df_rbind)))
 })
