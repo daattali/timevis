74418cf3af19695c36637e4f4c5eb1f1b90c7b84
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2019-01-30 13:56:30 +1100

    Changed encoding function to produce json objects

--- R/utils.R
+++ R/utils.R
@@ -24,7 +24,9 @@ dataframeToD3 <- function(df) {
   row.names(df) <- NULL
   lapply(seq_len(nrow(df)), function(row) {
     row <- df[row, , drop = FALSE]
-    lapply(row[, !is.na(row), drop = FALSE], as.character)
+    lapply(row[, !is.na(row), drop = FALSE], function(x){
+      return(as.character(jsonlite::toJSON(x, auto_unbox = TRUE)))
+    })
   })
 }
 
