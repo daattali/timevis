e3d4a777921ba9a16ce3119750dcc4d94c9fcdae
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2019-01-31 14:43:11 +1100

    Added unit test to check length-1 listing

--- tests/testthat/test-dataframeToD3.R
+++ tests/testthat/test-dataframeToD3.R
@@ -51,4 +51,13 @@ test_that("nested columns behave the way they ought to",{
   expect <- list(list(name = "Dean", age = "27"),
                  list(name = "Matt", age = "23", hobbies = as.list(matts_hobbies)))
   expect_identical(out, expect, info = info_comp(out, expect))
+
+  df2 <- df
+  expect2 <- expect
+
+  deans_hobby <- "Responding to pull requests"
+  df2$hobbies[1] <- list(deans_hobby)
+  expect2[[1]]$hobbies <- list(deans_hobby)
+  out2 <- dataframeToD3(df2)
+  expect_identical(out2, expect2, info = info_comp(out2, expect2))
 })
