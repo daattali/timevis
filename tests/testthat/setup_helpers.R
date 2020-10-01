a8c76a8030dda2d97322c4894c9772b07e1e3ec0
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-10-01 17:04:47 +1000

    Remove usage of glue package from unit test helpers

--- tests/testthat/setup_helpers.R
+++ tests/testthat/setup_helpers.R
@@ -1,10 +1,9 @@
 # Produce a more informative output when failing expect_equivalent/identical
 # testcases for visual comparison
 info_comp <- function(actual, expect){
-  if (!requireNamespace("glue", quietly = TRUE)){
-    return()
-  }
-  out <- glue::glue("Expect: {expect}",
-                    "Actual: {actual}", .sep = "\n")
-  return(out)
+  paste0(
+    c("Expect: ", "Actual: "),
+    c(expect, actual),
+    collapse = "\n"
+  )
 }
